d3d39558ba432ab4a3051b3264590d337747c576
Mihai-Codrin Robota <crobota99@gmail.com>
2025-02-09 17:03:40 +0200

    listings show/hide

--- src/app/pages/my-listings/components/RowTableMyListings.js
+++ src/app/pages/my-listings/components/RowTableMyListings.js
@@ -2,17 +2,41 @@ import RowTablecommon from "../../../shared/Table/RowTablecommon"
 import DeleteGame from "../../../features/delete-game/DeleteGame"
 import ViewEditGame from "../../../features/view-edit-game/ViewEditGame"
 import RowTableMyListingsOffers from "./RowTableMyListingsOffers"
+import React, {useState} from 'react'
+
+
 
 export default function RowTableMyListings(props){
 
     let rowData = props.rowData
     let currentPage = props.currentPage
 
+
+    const [ rowMyListings, setRowMyListings ] = useState(true);
+
+
+    // button to show/hide offers
+    let button 
+    if( rowMyListings) {
+        button = <button onClick={ () => setRowMyListings(false)} >  V </button>
+    } else {
+        button =  <button onClick={ () => setRowMyListings(true)} >  /\ </button>
+    }
+
+    //offers section
+    let offers 
+    if (rowMyListings) {
+        offers = <RowTableMyListingsOffers rowData={rowData.offers}/>
+    } else {
+        offers = <div className="hidden"/>
+    }
+
+
     return(
         <div className="flex flex-col row_table px-8 py-8" >
             <div className="flex">
-                {rowData.offers.length != 0 ?
-                    ( <button>  V </button> )
+                {rowData.offers.length != 0 ?                   
+                    ( button )
                     :
                     ( <div className="hidden"/> )
                 }
@@ -29,7 +53,7 @@ export default function RowTableMyListings(props){
             </div>
 
             {rowData.offers.length != 0 ?
-                    ( <RowTableMyListingsOffers rowData={rowData.offers}/> )
+                    ( offers )
                     :
                     ( <div className="hidden"/> )
                 }
