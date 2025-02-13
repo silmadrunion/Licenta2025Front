37f787070eda7fed32f1a8c2ab7893bf1ae090e9
Mihai-Codrin Robota <crobota99@gmail.com>
2025-02-13 12:15:32 +0200

    header profile display

--- src/app/pages/header/components/ProfileIcon.js
+++ src/app/pages/header/components/ProfileIcon.js
@@ -1,14 +1,35 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import React, {useState} from 'react'
 
+export default function ProfileIcon(props) {
 
-export default function ProfileIcon() {
+    const [ showProfile, setShowProfile ] = useState(false)
+
+    // let profileData = props.profile
+
     return (
-    <span> 
-        <button className='text-white px-2 py-2 mt-2'>
+    <span className='flex flex-col'> 
+        <button className='text-white px-2 py-2 mt-2  self-end' onClick={ ( ) => setShowProfile(!showProfile) }>
             <FontAwesomeIcon className="w-6 h-6" icon={faUser} />
             <FontAwesomeIcon className="w-6 h-6" icon={faCaretDown} />
         </button> 
+
+        { showProfile ? (
+            <div className='flex flex-col justify-start border-2 border-[#B1EDE8] rounded-lg absolute top-14 right-7 p-6 bg-[#352D39]'>
+                <div className='flex'>
+                    <img  style={{width:" 5.5rem"}} className=' h-32  border-2 border-[#B1EDE8]'></img>
+                    <div className='ml-4'> 
+                        <p className='text-2xl font-bold text-start'>name</p>
+                        <span className='text-center'>Rating number</span>
+                        <span></span>
+                    </div>
+                </div>
+                <button className='w-40 mt-2 text-start'>View Profile</button>
+                <button className='w-40 text-start'>Log Out</button>
+            </div>
+            ) 
+            :  ( <div className="hidden"/> )}
     </span>
     )
 }
