f8b1e4d617e2bb96704bf2d76bdc10a8b7082268
Mihai-Codrin Robota <crobota99@gmail.com>
2025-03-28 12:32:08 +0200

    small error fix add game side table

--- src/app/features/add-game/components/AddGameSideTable.js
+++ src/app/features/add-game/components/AddGameSideTable.js
@@ -16,12 +16,18 @@ return(
             <div className='flex flex-col mt-4'>
                 { 
                     AddGamesFromLibrary ? 
-                    AddGamesFromLibrary.map( game =>                     
-                    <div className='border-2 border-[#B1EDE8] flex p-2'>
+                    AddGamesFromLibrary.map( game =>    
+                        
+                    game.gameDetails !=undefined ? 
+                        <div className='border-2 border-[#B1EDE8] flex p-2'>
                         <input type='checkbox' className='rounded-lg'></input>
                         <img className='pl-4'></img>
                         <p className='pl-4'>{game.gameDetails.gameName}</p>
-                    </div> 
+                        </div> 
+                        :
+                        <div> </div>
+                                    
+
                     ) 
                     : <p> 
                     { action == 'addGame' ? "Seems like you have not added games yet into my Library. Please add games there and then you will be able to list them here." : "" } </p>               
